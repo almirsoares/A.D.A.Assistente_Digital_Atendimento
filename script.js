21560cf2e32dc03956b77d9d9b72b3c697d3eebf
testeASOD <almir.soares.grdtel@gmail.com>
2024-06-28 21:56:41 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function calcularProporcional() {
+    const valorPlano = parseFloat(document.getElementById('valorPlano').value);
+    const dataAntiga = new Date(document.getElementById('dataAntiga').value);
+    const dataNova = new Date(document.getElementById('dataNova').value);
+
+    if (isNaN(valorPlano) || isNaN(dataAntiga.getTime()) || isNaN(dataNova.getTime())) {
+        alert("Por favor, preencha todos os campos com valores válidos.");
+        return;
+    }
+
+    const diasBase = 30;
+    const diferencaDias = Math.floor((dataNova - dataAntiga) / (1000 * 60 * 60 * 24));
+    const diasTotais = diasBase + diferencaDias;
+
+    const valorProporcional = (valorPlano / diasBase) * diferencaDias;
+
+    document.getElementById('resultado').innerText = `Proporcional de Vencimento: R$ ${valorProporcional.toFixed(2)} para ${diasTotais} dias totais`;
+}
