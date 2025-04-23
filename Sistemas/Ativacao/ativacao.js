9f8b1e47390b8e9fe9ec3a20c1ff51b87f7997a9
almirsoares <almir.soares.grdtel@gmail.com>
2025-04-23 11:23:49 -0300

    ajuste de calculo de ativação

--- Sistemas/Ativacao/ativacao.js
+++ Sistemas/Ativacao/ativacao.js
@@ -11,27 +11,16 @@ function calcularAtivacao() {
     const dataAtivacao = new Date(document.getElementById('dataAtivacao').value + 'T00:00:00');
     const dataVencimento = new Date(document.getElementById('dataVencimento').value + 'T00:00:00');
 
-
-    // Seta a data para o calculo ser referente ao dia do vencimento anterior para calcular o proporcional decorrendo daquele dia
-    const dataParaCalculo1 = new Date(dataDesativacao);
-    dataParaCalculo1.setMonth(dataParaCalculo1.getMonth() - 1);
-    dataParaCalculo1.setDate(dataParaCalculo1.getDate() - 1);
-
     // Chama a função calcularProporcional para obter os cálculos
-    const prop1 = calcularProporcional(valorPlanoAnterior, dataParaCalculo1, dataInicio);
-    const desc1 = calcularProporcional(descontoPlanoAnterior, dataParaCalculo1, dataInicio);
+    const prop1 = calcularProporcional(valorPlanoAnterior, dataInicio, dataDesativacao);
+    const desc1 = calcularProporcional(descontoPlanoAnterior, dataInicio, dataDesativacao);
     const proporcional1 = prop1.valorTotal;
     const proporcionalDesc1 = desc1.valorTotal;
 
 
-    // Seta a data para o calculo ser referente ao dia do vencimento anterior para calcular o proporcional decorrendo daquele dia
-    const dataParaCalculo2 = new Date(dataVencimento);
-    dataParaCalculo2.setMonth(dataParaCalculo2.getMonth() - 1);
-    dataParaCalculo2.setDate(dataParaCalculo2.getDate() - 1);
-
     // Chama a função calcularProporcional para obter os cálculos
-    const prop2 = calcularProporcional(valorPlanoNovo, dataParaCalculo2, dataAtivacao);
-    const desc2 = calcularProporcional(descontoPlanoNovo, dataParaCalculo2, dataAtivacao);
+    const prop2 = calcularProporcional(valorPlanoNovo, dataAtivacao, dataVencimento);
+    const desc2 = calcularProporcional(descontoPlanoNovo, dataAtivacao, dataVencimento);
     const proporcional2 = prop2.valorTotal;
     const proporcionalDesc2 = desc2.valorTotal;
     
