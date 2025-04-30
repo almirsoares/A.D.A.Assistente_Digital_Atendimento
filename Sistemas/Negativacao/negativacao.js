3593bab6b1b1eee68fc7fd99d0800e62c02b01e0
Arthur <arthurkhlf00@gmail.com>
2025-04-30 12:45:55 -0300

    negativacao style & ajustes no js

--- Sistemas/Negativacao/negativacao.js
+++ Sistemas/Negativacao/negativacao.js
@@ -1,65 +1,65 @@
 function copiarCEP() {
-    const cep = document.getElementById("cep").value;
-    if (cep) {
-        navigator.clipboard.writeText(cep).then(() => {});
-    } else {
-        alert("Por favor, insira um CEP antes de copiar.");
-    }
+  const cep = document.getElementById("cep").value;
+  if (cep) {
+    navigator.clipboard.writeText(cep).then(() => {});
+  } else {
+    alert("Por favor, insira um CEP antes de copiar.");
+  }
 }
 
-function copiarTextoAlef(){
-    let textoAlef = 'MULTA DE EQUIPAMENTO GERADA, POIS A O.S. DE RETIRADA AINDA NÃO FOI FINALIZADA, CASO A CLIENTE DEVOLVA  O EQUIPAMENTO DESCONSIDERAR/DESATIVAR A MULTA E INFORMAR AO BACKOFFICE ';
-    textoAlef+='\nAUTORIZAÇÃO DIRETORIA ADMINISTRATIVA - ALEF';
-    navigator.clipboard.writeText(textoAlef).then(() => {})
+function copiarTextoAlef() {
+  let textoAlef = "MULTA DE EQUIPAMENTO GERADA, POIS A O.S. DE RETIRADA AINDA NÃO FOI FINALIZADA, CASO A CLIENTE DEVOLVA  O EQUIPAMENTO DESCONSIDERAR/DESATIVAR A MULTA E INFORMAR AO BACKOFFICE ";
+  textoAlef += "\nAUTORIZAÇÃO DIRETORIA ADMINISTRATIVA - ALEF";
+  navigator.clipboard.writeText(textoAlef).then(() => {});
 }
 
-function alertaNegativacao(){
-    const selectPlataforma = document.getElementById("plataforma").value;
-    if (!selectPlataforma) {
-        alert("Erro: O elemento select com id 'plataforma' não foi encontrado.");
-        return;
-    }
-    let alerta = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-    alerta +=  `\n\nCLIENTE ENCONTRA-SE NEGATIVADO(A) NO ${selectPlataforma}`;
-    alerta += '\nEm caso de pagamento, preencher o formulário';
-    alerta += '\n para solicitação de retirada do SPC/SERASA.';
-    alerta += '\n\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-    
-    navigator.clipboard.writeText(alerta).then(() => {});
+function alertaNegativacao() {
+  const selectPlataforma = document.getElementById("plataforma").value;
+  if (!selectPlataforma) {
+    alert("Erro: O elemento select com id 'plataforma' não foi encontrado.");
+    return;
+  }
+  let alerta = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+  alerta += `\n\nCLIENTE ENCONTRA-SE NEGATIVADO(A) NO ${selectPlataforma}`;
+  alerta += "\nEm caso de pagamento, preencher o formulário";
+  alerta += "\n para solicitação de retirada do SPC/SERASA.";
+  alerta += "\n\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+  navigator.clipboard.writeText(alerta).then(() => {});
 }
 
-function copiarNegativacao(){
-    const operador = document.getElementById('operador').value.trim();
-    const texto = document.getElementById('texto').value.trim();
+function copiarNegativacao() {
+  const operador = document.getElementById("operador").value.trim();
+  const texto = document.getElementById("texto").value.trim();
 
-    if (isNaN(operador) || operador === '') {
-        alert('Por favor, preencha o campo "operador".');
-        return;
-    }
-    if (texto === '') {
-        alert('Por favor, preencha o campo "texto" com os dados do SPC/SERASA.');
-        return;
-    }
+  if (operador.length <= 5) {
+    alert('Por favor, preencha o campo "operador".');
+    return;
+  }
+  if (operador.length <= 5) {
+    alert('Por favor, preencha o campo "texto" com os dados do SPC/SERASA.');
+    return;
+  }
 
-    let mensagem = 'CLIENTE NEGATIVADO!';
-    mensagem += '\nSMS DE  INCLUSÃO ENVIADO';
-    mensagem += `\nTRIAGEM: ${operador}`;
-    mensagem += `\n\n${texto}`;
+  let mensagem = "CLIENTE NEGATIVADO!";
+  mensagem += "\nSMS DE  INCLUSÃO ENVIADO";
+  mensagem += `\nTRIAGEM: ${operador}`;
+  mensagem += `\n\n${texto}`;
 
-    navigator.clipboard.writeText(mensagem).then(() => {});
+  navigator.clipboard.writeText(mensagem).then(() => {});
 }
 
-function copiarMultaONU(){
-    navigator.clipboard.writeText('MULTA ONU').then(() => {});
+function copiarMultaONU() {
+  navigator.clipboard.writeText("MULTA ONU").then(() => {});
 }
 
-function copiarAcordoNaoPago(){
-    navigator.clipboard.writeText('ACORDO NÃO PAGO').then(() => {});
+function copiarAcordoNaoPago() {
+  navigator.clipboard.writeText("ACORDO NÃO PAGO").then(() => {});
 }
 
-function copiarProtocoloRetirada(){
-    let protocolo = 'RETIRADA DE EQUIPAMENTOS E MATERIAIS\n';
-    protocolo += 'O.S. NÃO ABERTA OU NÃO FINALIZADA CORRETAMENTE APÓS DESATIVAÇÃO DE CADASTRO';
+function copiarProtocoloRetirada() {
+  let protocolo = "RETIRADA DE EQUIPAMENTOS E MATERIAIS\n";
+  protocolo += "O.S. NÃO ABERTA OU NÃO FINALIZADA CORRETAMENTE APÓS DESATIVAÇÃO DE CADASTRO";
 
-    navigator.clipboard.writeText(protocolo).then(() => {});
+  navigator.clipboard.writeText(protocolo).then(() => {});
 }
